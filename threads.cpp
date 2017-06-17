4741be30fe48babc25902b6f2f1c4f9ed5d7354e
OrangeKing <theeldu@gmail.com>
2017-06-17 21:03:54 +0200

    Initial mutex example

--- threads.cpp
+++ threads.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <vector>
+#include <mutex>
 
 int glob = 1337;
 
+long long sum = 0;
+std::mutex mx;
+
 void foo()
 {
-	std::this_thread::sleep_for(std::chrono::seconds(4));	//sleep(4);
-	std::cout << "foo done" << glob << std::endl;
+	for (int i = 0; i < 1000000; i++)
+	{
+		mx.lock();
+		sum += 7;
+		mx.unlock();
+	}
 }
 
 void bar()
@@ -18,12 +27,21 @@ void bar()
 
 int main()
 {
-	auto fooThread = std::thread(foo); //create new threads
-	auto barThread = std::thread(bar); 
+	std::vector<std::thread> threads;
 
-	fooThread.join();	//lock till the end of thread
-	barThread.join();
+	//10 watkow FOO
+	for (int i = 0; i < 10; i++)
+		threads.push_back(std::thread(foo));
+	
+	for (auto& thread : threads)
+		thread.join();
 
+	//auto fooThread = std::thread(foo); //create new threads
+	//auto barThread = std::thread(bar); 
+	//fooThread.join();	//lock till the end of thread
+	//barThread.join();
+
+	std::cout << sum << std::endl;
 	std::cout << "main done" << std::endl;
 
 	return 0;
