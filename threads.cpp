8ed8b4088b1dfc2932f7ccc3531bc0d072b74663
OrangeKing <theeldu@gmail.com>
2017-06-17 20:35:35 +0200

    Thread sleep example

--- threads.cpp
+++ threads.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 #include <thread>
+#include <chrono>
 
 int glob = 1337;
 
 void foo()
 {
+	std::this_thread::sleep_for(std::chrono::seconds(4));	//sleep(4);
 	std::cout << "foo done" << glob << std::endl;
 }
 
 void bar()
 {
+	std::this_thread::sleep_for(std::chrono::seconds(2));	//sleep(4);
 	std::cout << "bar done" << glob << std::endl;
 }
 
 int main()
 {
-	auto fooThread = std::thread(foo); //startuje nowy watek
-	auto barThread = std::thread(bar); //startuje nowy watek
+	auto fooThread = std::thread(foo); //create new threads
+	auto barThread = std::thread(bar); 
 
-	fooThread.join();	//blokuje az do zakonczenia danego watku
-	barThread.join();	//blokuje az do zakonczenia danego watku
+	fooThread.join();	//lock till the end of thread
+	barThread.join();
 
 	std::cout << "main done" << std::endl;
 
