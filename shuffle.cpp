685ec4cfdc7dfafac33f04e5c30f9f52e34be79c
OrangeKing <theeldu@gmail.com>
2017-06-20 17:52:34 +0200

    Shuffle functions updated

--- shuffle.cpp
+++ shuffle.cpp
@@ -4,24 +4,27 @@
 
 using namespace std;
 
-void shiftRight(vector<vector<int>>, int size);
-void shiftLeft(vector<vector<int>>, int size);
-
-void shiftUp(vector<vector<int>>, int size);
-void shiftDown(vector<vector<int>>, int size);
+void shiftRight(vector<vector<int>> &values);
+void shiftLeft(vector<vector<int>>	&values);
+void shiftUp(vector<vector<int>>	&values);
+void shiftDown(vector<vector<int>> 	&values);
 
 
 int main()
 {
-	vector<vector<int>> arr(5, vector<int>(5));	
-	arr[3][3] = 9;
-	arr[0][0] = 3;
+	vector<vector<int>> arr(4, vector<int>(8));	
+	arr[1][1] = 9;
+	arr[0][3] = 3;
+
+	cout <<arr.size() << endl;
+	cout <<arr[0].size() << endl;
+
 
 	cout << "BEFORE:" << endl;
 
 	for (int i = 0; i < arr.size(); i++)
 	{
-		for (int j = 0; j < arr.size(); j++)
+		for (int j = 0; j < arr[i].size(); j++)
 		{
 			cout << arr[i][j] << "   ";
 		}
@@ -31,21 +34,34 @@ int main()
 
 
 	cout << "AFTR:" << endl;
-	shiftLeft(arr,5);
+	shiftDown(arr);
+
+
+	for (int i = 0; i < arr.size(); i++)
+	{
+		for (int j = 0; j < arr[i].size(); j++)
+		{
+			cout << arr[i][j] << "   ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+	cout << endl;
 
 	return 0;
 }
 
-void shiftRight(vector<vector<int>> values, int size)
+void shiftRight(vector<vector<int>> &values)
 {
 	int temp;
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < values[i].size(); i++)
 	{
-		for (int j = 0; j < size; j++)
+		for (int j = 0; j < values.size(); j++)
 		{
 			if(values[j][i] != 0)
 			{
-				if(((i+1) != size) and (values[j][i+1] == 0))
+				if(((i+1) != values[i].size()) and (values[j][i+1] == 0))
 				{
 					temp = values[j][i];
 					values[j][i] = 0;
@@ -57,16 +73,16 @@ void shiftRight(vector<vector<int>> values, int size)
 	}
 }
 
-void shiftLeft(vector<vector<int>> values, int size)
+void shiftLeft(vector<vector<int>> &values)
 {
 	int temp;
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < values[i].size(); i++)
 	{
-		for (int j = 0; j < size; j++)
+		for (int j = 0; j < values.size(); j++)
 		{
 			if(values[j][i] != 0)
 			{
-				if(((i-1) > 0) and (values[j][i-1] == 0))
+				if(((i-1) >= 0) and (values[j][i-1] == 0))
 				{
 					temp = values[j][i];
 					values[j][i] = 0;
@@ -78,10 +94,44 @@ void shiftLeft(vector<vector<int>> values, int size)
 	}
 }
 
-// 		{
-// 			if(((i-1) > 0) and (values[i-1] == 0))
-// 			{
-// 				temp = values[i];
-// 				values[i] = 0;
-// 				values[i-1] = temp;
-// 				i++;
+void shiftUp(vector<vector<int>> &values)
+{
+	int temp;
+	for (int i = 0; i < values[i].size(); i++)
+	{
+		for (int j = 0; j < values.size(); j++)
+		{
+			if(values[j][i] != 0)
+			{
+				if(((j-1) >= 0) and (values[j-1][i] == 0))
+				{
+					temp = values[j][i];
+					values[j][i] = 0;
+					values[j-1][i] = temp;
+					i++;
+				}
+			}
+		}
+	}
+}
+
+void shiftDown(vector<vector<int>> &values)
+{
+	int temp;
+	for (int i = 0; i < values[i].size(); i++)
+	{
+		for (int j = 0; j < values.size(); j++)
+		{
+			if(values[j][i] != 0)
+			{
+				if(((j+1) != values[i].size()) and (values[j+1][i] == 0))
+				{
+					temp = values[j][i];
+					values[j][i] = 0;
+					values[j+1][i] = temp;
+					i++;
+				}
+			}
+		}
+	}
+}
